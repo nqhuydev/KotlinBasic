713a1602aad88a6166312f585b5f26facf2f11a7
Git Huy <gitusername>
2019-12-22 20:13:21 +0700

    If and When in Kotlin

--- src/2.When.kt
+++ src/2.When.kt
@@ -1,13 +1,26 @@
 fun main() {
     // Tháng thuốc Quí
-    var month : Int?
+    var month: Int?
     println("Nhập vào tháng cần tính: ")
     month = readLine()?.toInt()
-    when(month){
+    when (month) {
         1, 2, 3 -> println("Tháng $month thuộc quí 1.")
         4, 5, 6 -> println("Tháng $month thuộc quí 2.")
         7, 8, 9 -> println("Tháng $month thuộc quí 3.")
         10, 11, 12 -> println("Tháng $month thuộc quí 4.")
         else -> println("Số tháng nhập không hợp lệ.")
     }
-}git
+
+
+    // Tính độ tuổi
+    println("Tính độ tuổi, nhập tuổi cần tính: ")
+    var tuoi: Int? = 0
+    var s: String? = readLine()
+    if (s != null)
+        tuoi = s.toInt()
+
+    when(tuoi){
+        in 1..5 -> println("Tuổi thiếu nhi")
+
+    }
+}
