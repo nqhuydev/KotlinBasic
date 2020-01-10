51c3d1c4255e808148192ca4baaf6253b6e9120c
nqhuydev <Huy1@345678>
2020-01-10 22:45:26 +0700

    Hoan thanh bai tap ve Collections

--- src/19.BaiTapVeCollections.kt
+++ src/19.BaiTapVeCollections.kt
@@ -1,3 +1,9 @@
+/*
+*
+* Khai báo, các thao tác với Collections
+* Bài tập Quản lý sinh viên
+*
+* */
 fun main() {
 //   xulyCollections()
     quanLySinhVien()
@@ -60,49 +66,119 @@ private fun xulyCollections() {
 * */
 
 private fun quanLySinhVien() {
-    println("===== Quản Lý Sinh Viên =====")
-
-    println("1. Thêm Sinh viên")
-    println("2. Xuất ra danh sách Sinh viên")
-    println("3. Sửa Sinh viên")
-    println("4. Xóa Sinh viên có tên bất ký")
-    println("5. Tìm Sinh viên mà tên có chữ \'An\'")
-    println("6. Sắp xếp Sinh viên")
-    println("7. Xuất ra số lượng Sinh viên")
-
-    println("Nhập vào số của chức năng để chọn: ")
-    val so = readLine()?.toInt() ?: return
     val dsSinhVien = mutableListOf<String>()
-    when (so) {
-        1 -> {
-            while (true){
-                println("Nhập vào tên Sinh viên: ")
-                val ten = readLine() ?: return
-                dsSinhVien.add(ten)
-                println("Thêm sinh viên nữa không (c/k) ?")
-                val kt = readLine() ?: return
-                if (kt.compareTo("k", true) == 0){
-                    break
+
+    mainLoop@ while (true) {
+        println("\n")
+        println("===== Quản Lý Sinh Viên =====")
+        println("1. Thêm Sinh viên")
+        println("2. Xuất ra danh sách Sinh viên")
+        println("3. Sửa Sinh viên")
+        println("4. Xóa Sinh viên có tên bất ký")
+        println("5. Tìm Sinh viên theo chữ nhập vào")
+        println("6. Sắp xếp Sinh viên")
+        println("7. Xuất ra số lượng Sinh viên")
+
+        println("Nhập vào số của chức năng để chọn, nhập số khác bất kỳ để thoát: ")
+        val so = readLine()?.toInt() ?: return
+
+        when (so) {
+            1 -> {
+                println("===== Thêm Sinh viên mới =====")
+                myLoop@ while (true) {
+                    println("Nhập vào tên Sinh viên: ")
+                    val ten = readLine() ?: return
+                    dsSinhVien.add(ten)
+                    println("Thêm sinh viên nữa không (c/k) ?")
+                    val kt = readLine() ?: return
+                    if (kt.compareTo("k", true) == 0) {
+                        break@myLoop
+                    }
                 }
             }
-        }
-        2 -> {
+            2 -> {
+                println("===== Danh sách Sinh viên =====")
+                dsSinhVien.forEachIndexed { index, value ->
+                    println("${index + 1}. $value")
+                }
+            }
+            3 -> {
+                println("===== Sửa Thông tin Sinh viên =====")
+                dsSinhVien.forEachIndexed { index, value ->
+                    println("${index + 1}. $value")
+                }
+                println()
+                while (true) {
+                    println("Nhập vào số thứ tự cần thay đổi:")
+                    val soTT = readLine()?.toInt() ?: return
+                    if (0 < soTT && soTT <= dsSinhVien.size) {
+                        println("Bạn muốn đổi tên là gì: ")
+                        val ten = readLine() ?: return
+                        dsSinhVien[soTT - 1] = ten
+                        println("Thay đổi thành công!")
+                        break
+                    } else {
+                        println("Số bạn nhập không có trong danh sách. Nhập lại: ")
+                    }
+                }
 
-        }
-        3 -> {
-
-        }
-        4 -> {
-
-        }
-        5 -> {
-
-        }
-        6 -> {
-
-        }
-        7 -> {
-
+            }
+            4 -> {
+                println("===== Xóa Sinh viên =====")
+                while (true) {
+                    dsSinhVien.forEachIndexed { index, value ->
+                        println("${index + 1}. $value")
+                    }
+                    println()
+                    println("Nhập số Thứ tự ứng với Sinh viên cần xóa: ")
+                    val soTT = readLine()?.toInt() ?: return
+                    if (0 < soTT && soTT <= dsSinhVien.size) {
+                        dsSinhVien.removeAt(soTT - 1)
+                        println("Bạn có muốn xóa tiếp nữa ko (c/k)?")
+                        val kt = readLine() ?: return
+                        if (kt.compareTo("k", true) == 0) {
+                            break
+                        }
+                    } else {
+                        println("Số bạn nhập không có trong danh sách. Nhập lại: ")
+                    }
+                }
+            }
+            5 -> {
+                println("===== Tìm Sinh viên =====")
+                println("Nhập vào từ khóa để tìm:")
+                val tukhoa = readLine() ?: return
+                val dsTimThay = dsSinhVien.filter { it.contains(tukhoa, true) }
+                if (dsTimThay.isEmpty()) println("Không tìm thấy sinh viên nào với từ: \'$tukhoa\'")
+                else {
+                    println("Đã tìm thấy ${dsTimThay.size}: ")
+                    dsTimThay.forEachIndexed { index, value ->
+                        println("${index + 1}. $value")
+                    }
+                }
+            }
+            6 -> {
+                println("===== Sắp xếp Sinh viên =====")
+                println("Theo tên tăng dần: ")
+                dsSinhVien.sort()
+                dsSinhVien.forEachIndexed { index, value ->
+                    println("${index + 1}. $value")
+                }
+                println()
+                println("Theo tên giảm dần: ")
+                dsSinhVien.sortDescending()
+                dsSinhVien.forEachIndexed { index, value ->
+                    println("${index + 1}. $value")
+                }
+            }
+            7 -> {
+                println("===== Tổng số Sinh viên =====")
+                println("Có ${dsSinhVien.size} Sinh viên")
+            }
+            else -> {
+                println("Đã thoát chương trình!")
+                break@mainLoop // hay dùng return cũng đc
+            }
         }
     }
 }
